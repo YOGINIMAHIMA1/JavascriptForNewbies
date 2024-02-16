9ef86d79f92b686d7b9cbb58c11a55bf391740ab
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2024-02-16 10:17:20 +0530

    Create lesson3.js

---
+++ lesson3.js
@@ -0,0 +1,15 @@
+let age=18;
+switch(age)
+  {
+  case 18:
+   console.log("yes she is Adult");
+      break;
+    case 23:
+    console.log("she is not mature");
+      break;
+    case 45:
+    console.log("she is too adult");
+      break;
+  }
+
+      
